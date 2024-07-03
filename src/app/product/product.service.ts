a1b817a43d7452711276528cb9ed1424171d9bce
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-07-03 08:06:56 -0400

    product service

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl + "/products"; 
+
+  constructor(private http: HttpClient) {
+    
+   }
 }
