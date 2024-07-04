2e01c744a76358ad1968ac0c3e73bc43b80ffd16
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-07-04 19:28:52 -0400

    product list displaying products

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service'; // To import the getProducts() method
+import { Product } from '../../models/product';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  products: Product[] = [];
+
+  constructor(private productService: ProductService){} // instance of product service to load products onto lifecycle hook
+
+  ngOnInit(): void { // interface
+    // load products here
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    })
+  }
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +10,9 @@ export class ProductService {
 
   private apiUrl = environment.apiUrl + "/products"; 
 
-  constructor(private http: HttpClient) {
-    
-   }
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]>{ // type = observable and Product class (get array)
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
