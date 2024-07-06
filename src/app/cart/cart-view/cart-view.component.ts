29e02fef4f0701b28cfeb46ce74c53974f72e531
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-07-06 15:35:47 -0400

    angular material styling

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../../models/product';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list'
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-view',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatListModule, MatButtonModule, CommonModule],
   templateUrl: './cart-view.component.html',
   styleUrl: './cart-view.component.css'
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
+
+  cartItems: Product[] = [];
+  
+  constructor(private cartService: CartService){}
+
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(data => {
+      this.cartItems = data;
+    });
+  }
+
+
 
 }
