98279b44e631a8deb4c4ecb2b4858e95896bd076
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-07-06 15:40:54 -0400

    styling

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -16,15 +16,24 @@ import { CommonModule } from '@angular/common';
 export class CartViewComponent implements OnInit {
 
   cartItems: Product[] = [];
+
+  totalPrice: number = 0;
   
   constructor(private cartService: CartService){}
 
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     });
   }
 
-
+  getTotalPrice(): number{
+    let total = 0;
+    for(let item of this.cartItems){
+      total += item.price;
+    }
+    return total;
+  }
 
 }
